32b4c0abe17b2e3cb7e5e82e2c0ec770551a7527
Aliyanwear <aliyanwear@gmail.com>
2026-08-19 23:09:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,283 +1,82 @@
-// ALIYAN WEAR Constructor
+// ALIYAN WEAR
 
-let selectedColor = "white";
-let selectedStyle = "Oversize";
-let selectedSize = "M";
-let currentSide = "front";
-
-let frontDesign = "";
-let backDesign = "";
-
-const priceRussia = 2490;
-const priceTajikistan = 259;
-
-function updateTshirtImage() {
-    const tshirt = document.getElementById("tshirtImage");
-
-    if (selectedSide === "front") {
-        if (selectedColor === "white") {
-            tshirt.src = "tshirt-white.PNG";
-        } else {
-            tshirt.src = "tshirt-black.PNG";
-        }
-    }
-
-    if (selectedSide === "back") {
-        if (selectedColor === "white") {
-            tshirt.src = "tshirt-white-back.PNG";
-        } else {
-            tshirt.src = "tshirt-black-back.PNG";
-        }
-    }
-}
+let currentLanguage = "ru";
 
 
-// выбор цвета футболки
-function changeColor(color, button){
+const translations = {
 
-    selectedColor = color;
+ru:{
 
-    document.querySelectorAll(".color-button").forEach(btn=>{
-        btn.classList.remove("selected");
-    });
+title:"ALIYAN WEAR",
 
-    button.classList.add("selected");
+subtitle:"Создай свою уникальную футболку",
 
-    updateTshirtImage();
-}
+catalog:"Каталог футболок",
 
-function changeSide(side, button){
+catalogBtn:"Каталог",
 
-    selectedSide = side;
+designBtn:"Создать свой дизайн",
 
-    document.querySelectorAll(".side-button").forEach(btn=>{
-        btn.classList.remove("selected");
-    });
+buy:"Купить",
 
-    button.classList.add("selected");
+product:"Oversize T-shirt"
 
-    updateTshirtImage();
-}
+},
 
 
-    
-    
-    
+tj:{
 
-// выбор стиля
-function changeStyle(style,event){
+title:"ALIYAN WEAR",
 
-    selectedStyle = style;
+subtitle:"Футболкаи беназири худро созед",
 
-    document.querySelectorAll(".style-button").forEach(btn=>{
-        btn.classList.remove("selected");
-    });
+catalog:"Каталоги футболкаҳо",
 
-        event.classList.add("selected");
+catalogBtn:"Каталог",
 
-    console.log(
-        "Стиль:",
-        selectedStyle
-    );
-}
+designBtn:"Дизайни худро созед",
 
+buy:"Харидан",
 
-
-
-// выбор размера
-function changeSize(size, button){
-
-    selectedSize = size;
-
-    document.querySelectorAll(".size-button").forEach(btn=>{
-        btn.classList.remove("selected");
-    });
-
-    button.classList.add("selected");
-
-    console.log(
-        "Размер:",
-        selectedSize
-    );
+product:"Футболкаи Oversize"
 
 }
 
-
-// загрузка дизайна
-
-function uploadDesign(event){
-
-    const file = event.target.files[0];
-
-    if(!file) return;
+};
 
 
-    const reader = new FileReader();
 
 
-    reader.onload = function(e){
+function setLanguage(lang){
 
-        const design = document.getElementById("designPreview");
-
-if(currentSide === "front"){
-    frontDesign = e.target.result;
-}else{
-    backDesign = e.target.result;
-}
-
-design.src = e.target.result;
-    }
+    currentLanguage = lang;
 
 
-    reader.readAsDataURL(file);
-
-}
-
-
-// создание заказа
-
-function createOrder(){
-
-    let orderNumber =
-    Math.floor(10000 + Math.random()*90000);
+    document.getElementById("title").innerText =
+    translations[lang].title;
 
 
-    alert(
-    "Ваш заказ создан!\n\nALIYAN #" 
-    + orderNumber
-    );
+    document.getElementById("subtitle").innerText =
+    translations[lang].subtitle;
 
 
-    localStorage.setItem(
-        "order",
-        "ALIYAN #" + orderNumber
-    );
+    document.getElementById("catalogTitle").innerText =
+    translations[lang].catalog;
+
+
+    document.getElementById("catalogBtn").innerText =
+    translations[lang].catalogBtn;
+
+
+    document.getElementById("designBtn").innerText =
+    translations[lang].designBtn;
+
+
+    document.querySelector(".product button").innerText =
+    translations[lang].buy;
+
+
+    document.querySelector(".product h3").innerText =
+    translations[lang].product;
 
 }
-
-const design = document.getElementById("designPreview");
-
-let isMoving = false;
-let startX;
-let startY;
-let startLeft = 0;
-let startTop = 0;
-
-design.addEventListener("pointerdown", function(e){
-
-    isMoving = true;
-
-    startX = e.clientX;
-    startY = e.clientY;
-
-    startLeft = design.offsetLeft;
-    startTop = design.offsetTop;
-
-    design.setPointerCapture(e.pointerId);
-
-});
-
-
-design.addEventListener("pointermove", function(e){
-
-    if(!isMoving) return;
-
-    let x = startLeft + (e.clientX - startX);
-    let y = startTop + (e.clientY - startY);
-
-    design.style.left = x + "px";
-    design.style.top = y + "px";
-
-});
-
-
-design.addEventListener("pointerup", function(){
-
-    isMoving = false;
-
-});
-
-
-
-let scale = 1;
-let startDistance = 0;
-
-
-design.addEventListener("touchstart", function(e){
-
-    if(e.touches.length === 2){
-
-        startDistance = getDistance(
-            e.touches[0],
-            e.touches[1]
-        );
-
-    }
-
-});
-
-
-design.addEventListener("touchmove", function(e){
-
-    if(e.touches.length === 2){
-
-        let distance = getDistance(
-            e.touches[0],
-            e.touches[1]
-        );
-
-
-        let zoom = distance / startDistance;
-
-
-        scale = scale * zoom;
-
-
-        if(scale < 0.5){
-            scale = 0.5;
-        }
-
-
-        if(scale > 3){
-            scale = 3;
-        }
-
-
-        design.style.width = 
-        (120 * scale) + "px";
-
-
-        startDistance = distance;
-
-    }
-
-});
-
-
-function getDistance(a,b){
-
-    let x = a.clientX - b.clientX;
-    let y = a.clientY - b.clientY;
-
-    return Math.sqrt(x*x+y*y);
-
-}
-
-function switchSide(side, button){
-
-    currentSide = side;
-
-    document.querySelectorAll(".side-button").forEach(btn=>{
-        btn.classList.remove("selected");
-    });
-
-    button.classList.add("selected");
-
-    console.log("Тараф:", currentSide);
-}
-
-
-
-
-
-
